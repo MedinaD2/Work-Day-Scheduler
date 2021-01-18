eabfdb2c3337c4aac671f1cd060c5bbe7de579f7
Devannah <dmedina@Devannahs-MacBook-Pro.local>
2021-01-17 22:03:10 -0700

    UX enhancements

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Work-Day-Scheduler
+
+https://medinad2.github.io/Work-Day-Scheduler/
+
+# Description 
+ A JS app that you can personal use to schdule different tasks throughout your day.
+
+# User Story
+
+As an employee and bootcamp student an easy to use agenda can help orangize my days and become more efficient on completing everyday tasks.
+
+# Frameworks
+  - JS
+  - HTML
+  - CSS
+
+
+# Screenshot 
--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,1 +1,70 @@
-var 
+
+var now = dayjs().hour()
+
+// Variable to hold the hours and events and reload any tasks saved in local storage
+var tasks = ["", "", "", "", "", "", "", "",]
+    if (localStorage.getItem("tasks")){
+        tasks = JSON.parse(localStorage.getItem("tasks"))
+    }
+
+// Save items in LS
+localStorage.setItem("tasks", JSON.stringify(tasks))
+
+// From LS
+var mySavedTasks = JSON.parse(localStorage.getItem("tasks"));
+mySavedTasks.forEach( function(element, index) {
+document.getElementById(index+"textarea").value = element
+})
+
+// WHEN BUTTON IS CLICKED ITEMS ARE SAVED
+$(".saveBtn").click(function(event){
+    var buttonId = event.currentTarget.id
+    var newTask = document.getElementById(buttonId[0] + "textarea").value
+    var taskNumber = parseInt(buttonId[0]);
+    tasks[taskNumber]=newTask;
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+})
+
+//  CURRENT DAY
+$('#currentDay').text("Today is: " + moment().format('dddd') + ", " + moment().format('MMMM Do YYYY'));
+
+//  EDIT BLOCK
+$(".description").on("click", "p", function() {
+    
+    var text = $(this)
+      .text()
+      .trim();
+  
+    // replace p element with a new textarea
+    var textInput = $("<textarea>")
+      .addClass("form-control")
+      .val(text);
+    $(this).replaceWith(textInput);
+  
+  
+    textInput.trigger("focus");
+  });
+
+// CURRENT TIME AND AGENDA 
+$(".time-block").each(function () {
+    var agendaHour = $(this).attr("id");
+
+    
+    if (agendaHour < now) {
+    $(this).addClass("past");
+    $(this).removeClass("present");
+    $(this).removeClass("future");
+    }
+    
+    else if (agendaHour == now) {
+    $(this).removeClass("past");
+    $(this).addClass("present");
+    $(this).removeClass("future");
+    }
+    
+    else {
+    $(this).removeClass("past");
+    $(this).removeClass("present");
+    $(this).addClass("future");
+    }
+})
